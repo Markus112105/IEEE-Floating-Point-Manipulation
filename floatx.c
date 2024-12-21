92a5a8616a539f86ecd5cc31ee4f891572047564
Markus Massina <131615308+Markus112105@users.noreply.github.com>
2024-12-21 13:29:22 -0500

    Update floatx.c

--- floatx.c
+++ floatx.c
@@ -7,25 +7,15 @@
 floatx doubleToFloatx(double val,int totBits,int expBits) {
 
 /*-----------------------------------------------------------------------------------------------
-	From the README: 	At a high level, doing the conversion requires several manipulations:
 	1. Extracting the sign bit from the double value, and inserting it into the floatx value at
 		the correct position.
 	2. Extract the biased exponent from the double value.
-			Check to see if the double value is sub-normal.
-			Check to make sure the floatx exponent won't overflow or underflow).
-					If so, handle as a special case.
-			If not, rebias the exponent using the floatx bias (which depends on the number of exponent bits)
-			and write the result to the correct location in the floatx result.
 	3. Extract the fraction bits from the double value.
-			Determine how many bits are available for the fraction in the floatx value,
-			and truncate or extend the original value,
-			and write the resulting bits to the floatx result.
 	4. Handle special cases, such as infinity, or not-a-number.		
-	5. Return the floatx result.
+	5. Return result.
 ----------------------------------------------------------------------------------------------------*/
 
-	// First, make some assertions to ensure the totBits and expBits parameters are OK
-	// Then, implement the algorithm
+	// Make assertions then implement algorithim
 	assert(totBits >= 3 && totBits <= 64);
 	assert(expBits >= 1 && expBits <= totBits);
 
